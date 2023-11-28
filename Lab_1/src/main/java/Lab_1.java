67dd0e25754535dce698ac3d7c8bd1624b53b633
dev68sft <dev68sft@gmail.com>
2023-11-28 11:59:01 +0200

    Lab 1 Step 1

---
+++ Lab_1/src/main/java/Lab_1.java
@@ -0,0 +1,9 @@
+public class Lab_1 {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Lab_1");
+        System.out.println("Step 1");
+
+    }
+}
