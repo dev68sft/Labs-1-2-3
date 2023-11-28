d75217b07f9f7440cf37ff6d1889ae597e11e95c
dev68sft <dev68sft@gmail.com>
2023-11-28 12:01:37 +0200

    Lab 1 Step 3

--- Lab_1/src/main/java/Lab_1.java
+++ Lab_1/src/main/java/Lab_1.java
@@ -3,7 +3,7 @@ public class Lab_1 {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Lab_1");
-        System.out.println("Step 2");
+        System.out.println("Step 3");
 
     }
 }
